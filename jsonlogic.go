1d3f3b1b3afc8f449b9d5b9afc5823c923ad2a68
Restia <1625016239@qq.com>
2019-12-24 17:51:56 +0800

    修改除法中首个数字的判定方式

--- jsonlogic.go
+++ jsonlogic.go
@@ -235,8 +235,8 @@ func mult(values interface{}) interface{} {
 func div(values interface{}) interface{} {
 	var sum float64
 
-	for _, n := range values.([]interface{}) {
-		if sum == 0 {
+	for i, n := range values.([]interface{}) {
+		if i == 0 {
 			sum = toNumber(n)
 
 			continue
